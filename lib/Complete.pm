4eb8490107f8e7c44b1befec5ebb76c9a4ed001f
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2014-11-27 07:37:11 +0700

    Revise POD: give explanation on completion structure, remove Developer's Notes section, and [incompat] change key 'completion' to 'words'

--- lib/Complete.pm
+++ lib/Complete.pm
@@ -12,44 +12,68 @@ use 5.010001;
 
 The namespace C<Complete::> is used for the family of modules that deal with
 completion (including, but not limited to, shell tab completion, tab completion
-feature in other CLI-based application, web autocomplete, etc).
+feature in other CLI-based application, web autocomplete, completion in GUI,
+etc). Compared to other modules, this (family of) module(s) tries to have a
+clear separation between general completion routine and shell-/environment
+specific ones, for more reusability.
+
+This POD page gives an overview of the modules in C<Complete::*> namespace.
+
+=head2 Modules
 
 C<Complete::Bash::*> modules are specific to bash shell. See L<Complete::Bash>
-on how to do bash tab completion with Perl.
+on some of the ways to do bash tab completion with Perl. Other shells are also
+supported. For shell-specific information, please refer to C<Complete::Zsh>,
+C<Complete::Tcsh>, C<Complete::Fish>.
 
-C<Complete::Zsh::*> modules are specific to zsh shell. See L<Complete::Zsh> on
-how to do zsh tab completion with Perl.
+Other modules usually are named after the type of completion answer they
+provide. For example: L<Complete::Unix> completes username/group name,
+L<Complete::Util> completes from several generic sources: C<complete_array_elem>
+completes from an array, C<complete_env> from a list of environment variables,
+C<complete_file> from files/directories on the filesystem.
 
-C<Complete::Fish::*> modules are specific to fish shell. See L<Complete::Fish>
-on how to do fish tab completion with Perl.
-
-Compared to other modules, this (family of) module(s) tries to have a clear
-separation between general completion routine and shell-/environment specific
-ones, for more reusability.
+C<Complete::*> modules for non-shell environment (like browser or GUI) have not
+been developed. Please check again from time to time in the future.
 
 =head2 C<complete_*()> functions
 
 The main functions that do the actual completion are the C<complete_*()>
 functions. These functions are generic completion routines: they accept the word
-to be completed, zero or more other arguments, and mostly return an arrayref
-containing possible completion from a specific source. They are not tied to any
-environment (shell, or web). They can even be used for general purposes outside
-the context of completion. Examples are C<complete_file()> (complete from list
-of files in a specific directory), C<complete_env()> (complete from list of
-environment variables), and so on. An example:
+to be completed, zero or more other arguments, and return a completion answer
+structure (see L</"Completion answer structure">).
 
  use Complete::Util qw(complete_array_elem);
  my $ary = complete_array_elem(array=>[qw/apple apricot banana/], word=>'ap');
  # -> ['apple', 'apricot']
 
-More complex C<complete_*()> functions might return a I<hashref> instead of
-arrayref: it contains the completion reply (in the C<completion> keys) as well
-as other metadata like hints so the formatting function can properly display
-this to shell/etc. Example:
+=head2 Completion answer structure
 
- {completion=>[qw/$HOME $ENV/], type=>'env'}
+C<complete_*()> functions can return an arrayref or hashref. An example of the
+arrayref form:
 
-Given this data, C<Complete::Bash::format_completion()> will produce this:
+ ['apple', 'apricot']
+
+That is, the completion answer is simply an array of words. However, each
+element can also be a hashref to give metadata to each entry, for example:
+
+ [{word=>'apple', summary=>'A delicious fruit with thousands of varieties'},
+  {word=>'apricot', summary=>'Another delicious fruit'},]
+
+The C<summary> can be used, e.g. by the fish shell which displays a description
+beside each completion. Or by other environments as they see fit. Other metadata
+can also be added to each entry's hashref.
+
+The second form of completion answer structure is hashref. It must contain the
+main key C<words> which is the same structure as the arrayref above. Aside from
+C<words>, the hashref can contain extra metadata which can give hints to the
+formatter on how to better format/display the answer: C<type>, C<path_sep>,
+C<escmode> etc (see L<Complete::Bash> for example on what hints it understands).
+
+ {words=>[qw/apple apricot/]}
+ {words=>[qw/$HOME $ENV/], type=>'env'}
+
+In the second example, C<Complete::Bash::format_completion()> can be instructed
+to produce the final result like this:
 
  $HOME
  $ENV
@@ -57,60 +81,16 @@ Given this data, C<Complete::Bash::format_completion()> will produce this:
 However, given one of these:
 
  [qw/$HOME $ENV/]
- {completion=>[qw/$HOME $ENV/], type=>'filename'}
+ {words=>[qw/$HOME $ENV/], type=>'filename'}
 
 then C<format_completion()> will produce:
 
  \$HOME
  \$ENV
 
-the difference is the escaping backslash for the C<$> character. There are other
-hints available.
-
-
-=head1 DEVELOPER'S NOTES
-
-B<Metadata>. C<complete_*()> currently returns array of string. In some
-environment like fish shell this does not provide enough metadata. In fish,
-aside from string, each completion alternative has some extra metadata. For
-example, when completing filenames, fish might show each possible completion
-filename with type (file/directory) and file size at the right of each file
-name. When completing options, it can also display a summary text for each
-option. So instead of an array of strings, in the future C<complete_*()> can
-also return an array of hashrefs:
-
- ["word1", "word2", "word3"]
-
-versus:
-
- [ {word=>"word1", ...},
-   {word=>"word2", ...},
-   {word=>"word3", ...}, ]
-
-B<Matching method>. Currently most C<complete_*()> routines match word using
-simple string prefix matching. fish also supports matching not by prefix only,
-but using wildcard. For example, if word if C<b??t> then C<bait> can be
-suggested as a possible completion. fish also supports fuzzy matching (e.g.
-C<house> can bring up C<horse> or C<hose>). There is also
-spelling-/auto-correction feature in some shells. This mode of matching can be
-added later in the various C<complete_*()> routines, turned on via a flag
-argument. Or there could be helper routines for this. In general this won't pose
-a problem to the API.
-
-B<Autosuggest>. fish supports autosuggestion (autocomplete). When user types,
-without she pressing Tab, the shell will suggest completion (not only for a
-single token, but possibly for the entire command). If the user wants to accept
-the suggestion, she can press the Right arrow key. This can be supported later
-by a function in L<Complete::Fish> e.g. C<shell_complete()> which accepts the
-command line string.
+the difference is the escaping backslash for the C<$> character.
 
 
 =head1 SEE ALSO
-
-L<Bash::Completion>, L<Getopt::Complete>, L<Term::Completion>
-
-L<Perinci::Sub::Complete> and C<Perinci::Sub::Complete::*> modules deal with
-providing completion capability for functions that have L<Rinci> metadata.
-
 
 =cut
