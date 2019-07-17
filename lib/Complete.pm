7014ee3a0473d77c79036b73dd3dcffc6230d518
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-07-17 22:54:02 +0700

    Some wording and ordering tweaks

--- lib/Complete.pm
+++ lib/Complete.pm
@@ -11,8 +11,8 @@ package Complete;
 The namespace C<Complete::> is used for the family of modules that deal with
 completion (including, but not limited to, shell tab completion, tab completion
 feature in other CLI-based application, web autocomplete, completion in GUI,
-etc). This (family of) modules try to have a clear separation between general
-completion routine and shell-/environment specific ones, for more reusability.
+etc). These modules try to have a clear separation between general completion
+routine and shell-/environment specific ones, for more reusability.
 
 This POD page establishes convention and gives an overview of the modules in
 C<Complete::*>.
@@ -86,17 +86,11 @@ See L</"Completion answer structure">.
 
 =head2 Completion answer structure
 
-C<complete_*()> functions return completion answer structure. Completion answer
+C<complete_*()> functions return completion answer structure. This structure
 contains the completion entries as well as extra metadata to give hints to
 formatters/tools. It is a hashref which can contain the following keys:
 
 =over
-
-=item * message => string
-
-Experimental. Instead of returning completion entries, a completion answer can
-also opt to request showing a message (i.e. error message, or informational
-message) to the user.
 
 =item * words => array
 
@@ -108,10 +102,11 @@ or a hashref. Example:
  [{word=>'apple', summary=>'A delicious fruit with thousands of varieties'},
   {word=>'apricot', summary=>'Another delicious fruit'},] # array of hashes
 
-As you can see from the above, each entry specify the B<word> and can also
-contain additional information: B<summary> (str, short one-line description, can
-be displayed e.g. in shells that support them, like fish and zsh), B<is_partial>
-(bool, specify whether this is a partial completion).
+As you can see from the above, each entry specifies the B<word> and can also
+contain additional information: B<summary> (str, short one-line description
+about the entry, can be displayed alongside the entry), B<is_partial> (bool,
+specify whether this is a partial completion which means the word is not the
+full entry).
 
  # example of digit-by-digit completion
  [
@@ -129,26 +124,32 @@ entries.
 
 =item * type => str
 
-See L<Complete::Bash>.
+For now, see L<Complete::Bash> about the various types supported.
 
 =item * path_sep => str
 
-See L<Complete::Bash>.
+For now, see L<Complete::Bash>.
 
 =item * esc_mode => str
 
-See L<Complete::Bash>.
+For no, see L<Complete::Bash>.
 
 =item * static => bool
 
-Specify that completion is "static", meaning that it does not depend on external
-state (like filesystem) or a custom code which can return different answer
-everytime completion is requested.
+Specifies that completion is "static", meaning that it does not depend on
+external state (like filesystem) or a custom code which can return different
+answer everytime completion is requested.
 
 This can be useful for code that wants to generate completion code, like bash
 completion or fish completion. Knowing that completion for an option value is
 static means that completion for that option can be answered from an array
 instead of having to call code/program (faster).
+
+=item * message => string
+
+Experimental. Instead of returning completion entries, a completion answer can
+also opt to request showing a message (i.e. error message, or informational
+message) to the user.
 
 =back
 
